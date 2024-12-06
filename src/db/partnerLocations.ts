451c46711786dddd2c6894a1c6f35d3e18aa9d4f
za-Salman <salman@geekinformatic.com>
2024-12-06 11:14:25 +0530

    partner location schema

---
+++ src/db/partnerLocations.ts
@@ -0,0 +1,59 @@
+import * as mongoose from 'mongoose';
+import { GENDERS} from '../constants';
+
+const partnerLocationSchema = new mongoose.Schema({
+  // partner
+  address: {
+    type: String,
+    default: ""
+  },
+  city: {
+    type: String,
+    default: ""
+  }, 
+  state: {
+    type: String,
+    default: ""
+  },
+  phone: {
+    type: String
+  },
+  images: [{
+    type: String,
+    default: ""
+  }],
+  servivces: [
+    {
+      type: mongoose.Schema.Types.ObjectId
+    }
+  ],
+  date: {
+    type: Date,
+    default: null
+  },
+  startTime: {
+    type: String,
+    default: ''
+  },
+  endTime: {
+    type: String,
+    default: ''
+  },
+  createdBy: {
+    type: mongoose.Schema.Types.ObjectId
+  },
+  isDeleted: {
+    type: Boolean,
+    default: false
+  }
+},
+  { timestamps: true });
+
+  partnerLocationSchema.set('toJSON', {
+  virtuals: false, transform: (doc, ret, Options) => {
+    delete ret.password
+    delete ret.__v
+  }
+})
+
+export const PartnerModel = mongoose.model('partners', partnerLocationSchema);
